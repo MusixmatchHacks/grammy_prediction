5c729a0402fe03bdc265a9da5fe9cc9acd46bcf4
neo01124 <varunjewalikar@hotmail.com>
2016-02-05 11:24:23 +0100

    nominees/winner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,3 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var track_template = Handlebars.compile($('#template-chart-track').html());
+
+$('#winners_nominees_player').append(track_template(all_data['14743783']));
